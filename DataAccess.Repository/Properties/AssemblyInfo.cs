fb5be315d4629581a4e47bc31621981f067e309f
nikita titarenko <nt@poloniumarts.com>
2018-03-20 08:43:56 +0300

    + DataAccess.Repository.nuspec

--- DataAccess.Repository/Properties/AssemblyInfo.cs
+++ DataAccess.Repository/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Racetrack.DataAccess")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DataAccess.Repository")]
+[assembly: AssemblyDescription("Convinient EF6 based Repository")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Racetrack.DataAccess")]
+[assembly: AssemblyCompany("nikita.titarenko")]
+[assembly: AssemblyProduct("Nt.DataAccess")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
