218c761db51d69e2b24b933cf3c9bc34ba895c5b
nikita titarenko <nt@poloniumarts.com>
2018-03-30 15:45:33 +0300

    + DataAccess.Client.EFCore

---
+++ DataAccess.Client.EFCore/ContextFactory.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Client.EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Client.EFCore
+{
+    public class ContextFactory
+    {
+        private List<User> _users;
+        private List<Blog> _blogs;
+        private List<Post> _posts;
+        private List<string> _tags;
+        private Random _r;
+        private BlogContext _ctx;
+
+        public ContextFactory()
+        {
+            _users = new List<User>();
+            _blogs = new List<Blog>();
+            _posts = new List<Post>();
+
+            _tags = new List<string>()
+            {
+                "health",
+                "sport",
+                "love",
+                "war",
+                "toys"
+            };
+
+            _r = new Random();
+        }
+
+        public BlogContext GetContext()
+        {
+            var ctx = new BlogContext(
+                new DbContextOptionsBuilder<BlogContext>()
+                .UseInMemoryDatabase("Blogs")
+                .Options);
+
+            return InitDatabase(ctx);
+        }
+
+        private BlogContext InitDatabase(BlogContext ctx)
+        {
+            InitUsers();
+            ctx.Users.AddRange(_users);
+            ctx.SaveChanges();
+
+            InitBlogs();
+            ctx.Blogs.AddRange(_blogs);
+            ctx.SaveChanges();
+
+            InitPosts();
+            ctx.Posts.AddRange(_posts);
+            ctx.SaveChanges();
+
+            return ctx;
+        }
+
+        private void InitUsers()
+        {
+            _users.Add(new User() { Name = "Nikita", RegistrationDate = DateTime.Today });
+            _users.Add(new User() { Name = "Ivan", RegistrationDate = DateTime.Today });
+            _users.Add(new User() { Name = "Maxim", RegistrationDate = DateTime.Today });
+            _users.Add(new User() { Name = "Sergey", RegistrationDate = DateTime.Today });
+            _users.Add(new User() { Name = "Nikolay", RegistrationDate = DateTime.Today });
+        }
+
+        private void InitBlogs()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var author = _users[_r.Next(_users.Count)];
+                _blogs.Add(new Blog()
+                {
+                    Name = GetRandomText(10),
+                    Author = author,
+                    CreateDate = DateTime.Today
+                });
+            }
+        }
+
+        private void InitPosts()
+        {
+            for (int i = 0; i < 2000; i++)
+            {
+                var blog = _blogs[_r.Next(_blogs.Count)];
+                var tag = _tags[_r.Next(_tags.Count)];
+
+                _posts.Add(new Post()
+                {
+                    Name = GetRandomText(20),
+                    BlogId = blog.Id,
+                    Tag = tag,
+                    PostDate = DateTime.Now.AddDays(_r.Next(1000) * -1)
+                });
+            }
+        }
+
+        private string GetRandomText(int length)
+        {
+            var dict = "abcdefghijklmnopqrstuvwxyz1234567890";
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                var symbol = dict[_r.Next(dict.Length)];
+                text.Append(symbol);
+            }
+
+            return text.ToString();
+        }
+    }
+}
---
+++ DataAccess.Client.EFCore/Models/BlogContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Client.EFCore.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+            //Database.SetInitializer(new BlogInitializer());
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+    }
+}
